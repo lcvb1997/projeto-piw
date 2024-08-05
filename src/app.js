9f955398e1e4d10cd60a762bfe28ecb0ebd0dbd9
lcvb1997 <lcvb1997@alu.ufc.br>
2024-08-05 11:56:18 -0300

    express utilizado e nodemon adicionado ao package.json

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+
+const port = 8090;
+
+app.get('/', function (req, res){
+    res.send("hello world");
+})
+
+
+app.listen(port, ()=> console.log("api rodando na porta",`${port}`))
+
