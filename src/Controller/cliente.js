d6f78a242827a7ce902efbce6b4deace10524a46
DanielJBE <125697208+DanielJBE@users.noreply.github.com>
2024-09-02 16:42:34 -0300

    Add files via upload
    Vai dar bom, confia

--- src/Controller/cliente.js
+++ src/Controller/cliente.js
@@ -6,36 +6,54 @@ export async function createTable(){
         db.exec('CREATE TABLE IF NOT EXISTS Cliente (id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER)');
     })
 }
-//Inserir informações no banco de dados
-export async function insertCliente(cliente){
-    openDb().then(db=>{
-        db.run('INSERT INTO Cliente (nome, idade) VALUES (?,?)', [cliente.nome, cliente.idade]);
-    })
-}
-//Atualizar informações no banco de dados
-export async function updateCliente(cliente){
-    openDb().then(db=>{
-        db.run('UPDATE Cliente SET nome=?, idade=? WHERE id=?', [cliente.nome, cliente.idade, cliente.id]);
-    })
-}
+
 //Mostrar informações no banco de dados
-export async function selectClientes(cliente){
-    return openDb().then(db=>{
-        return db.all('SELECT * FROM Cliente').then(res=>res)
+export async function selectClientes(req, res){
+    openDb().then(db=>{
+        db.all('SELECT * FROM Cliente').then(clientes=>res.json(clientes));
     })
 }
 
 //Mostrar informações sobre um cliente no banco de dados
-export async function selectCliente(id){
-    return openDb().then(db=>{
-        return db.get('SELECT * FROM Cliente WHERE id=?',[id] ).then(res=>res)
+export async function selectCliente(req, res){
+    let id = req.body.id;
+    openDb().then(db=>{
+        db.get('SELECT * FROM Cliente WHERE id=?',[id] ).then(cliente=>res.json(cliente));
     })
 }
 
+//Inserir informações no banco de dados
+export async function insertCliente(req, res){
+    let cliente = req.body;
+    openDb().then(db=>{
+        db.run('INSERT INTO Cliente (nome, idade) VALUES (?,?)', [cliente.nome, cliente.idade
+
+        ]);
+        res.json({
+            "Statuscode": 200
+        })
+    })
+}
+//Atualizar informações no banco de dados
+export async function updateCliente(req, res){
+    let cliente = req.body;
+    openDb().then(db=>{
+        db.run('UPDATE Cliente SET nome=?, idade=? WHERE id=?', [cliente.nome, cliente.idade, cliente.id
+
+        ]);
+        res.json({
+            "Statuscode": 200
+        })
+    })
+}
 
 //Excluir informações no banco de dados
-export async function deleteCliente(id){
-   return openDb().then(db=>{
-        return db.get('DELETE FROM Cliente WHERE id=?', [id]).then(res=>res);
+export async function deleteCliente(req, res){
+    let id = req.body.id;
+   openDb().then(db=>{
+        db.get('DELETE FROM Cliente WHERE id=?', [id]).then(res=>res);
+    });
+    res.json({
+        "Statuscode": 200
     })
 }
--- src/app.js
+++ src/app.js
@@ -1,63 +1,23 @@
-import {openDb} from './configDatabase.js';
-import { createTable, insertCliente, updateCliente, selectClientes, selectCliente, deleteCliente } from './Controller/cliente.js'
 import express from 'express';
-
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-createTable();
+//Importando rotas
+import router from './routes.js'
+app.use(router);
 
-//abrir conexão com o banco de dados SQlite
-
+//Qual porta a api está rodando
 const port = 8090;
+app.listen(port, ()=> console.log("Api rodando na porta",`${port}`))
 
-//app.get('/', function (req, res){
-//    res.send("hello world");
-//})
 
-//rota para mostrar todos os clientes
-app.get('/clientes', async function (req, res){
-    let clientes = await selectClientes();
-    res.json(clientes);
-    
-})
-//rota para mostrar um só cliente
-app.get('/cliente', async function (req, res){
-    let clientes = await selectCliente(req.body.id);
-    res.json(clientes);
-    
-})
-//rota para criar um cliente
-app.post('/cliente', function(req, res){
-    
-    insertCliente(req.body)
-    res.json({
-        "statuscCode": 200
-    })
-});
-//rota para atualizar o cliente
-app.put('/cliente', function(req, res){
-
-    if(req.body && !req.body.id){
-        res.json({
-            "statusCode": "400",
-            "msg": "Você precisa informar um id!"
-        })
-    }else{
-        updateCliente(req.body)
-        res.json({
-            "statusCode": 200
-        })
-    }
-    
-});
-//Rota para excluir o cliente
-app.delete('/cliente', async function(req, res){
-    
-    let cliente = await deleteCliente(req.body.id)
-    res.json(cliente);
-});
-
-app.listen(port, ()=> console.log("api rodando na porta",`${port}`))
-
+//apenas caso necessite de um certificado
+ https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(8091, ()=> console.log("Api rodando no https"));
