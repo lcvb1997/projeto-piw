7c040f0e34a02180062d48c84be0155ee10dae6e
Lucas Viana <61316178+lcvb1997@users.noreply.github.com>
2024-09-21 11:22:09 -0300

    Delete backend directory

--- backend/src/@types/tipos.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-declare global {
-    namespace Express {
-        interface Request {
-            user?: any; // Ou um tipo mais específico, se você souber
-        }
-    }
-}
