caa57293c363c979019fc2d82794764dc092a357
Lucas Viana <61316178+lcvb1997@users.noreply.github.com>
2024-09-22 16:02:39 -0300

    deletando pasta duplicada

--- Front-end/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new (require('webpack')).DefinePlugin({
-        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(true),
-      }),
-    ],
-  },
-}
